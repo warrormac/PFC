72ebe591d97bf8bf90dc07ad5946af4fcf1384f7
Andre Alvarez <andre.alvarez@ucsp.edu.pe>
2023-07-10 00:41:24 -0500

    Update multi_head_attention_2.py

--- Tesis v2/multi_head_attention_2.py
+++ Tesis v2/multi_head_attention_2.py
@@ -75,18 +75,18 @@ class MultiHeadAttention(Layer):
     
     
 ################################# TEST ##################################
-from numpy import random
+# from numpy import random
  
-input_seq_length = 5  # Maximum length of the input sequence
-h = 8  # Number of self-attention heads
-d_k = 64  # Dimensionality of the linearly projected queries and keys
-d_v = 64  # Dimensionality of the linearly projected values
-d_model = 512  # Dimensionality of the model sub-layers' outputs
-batch_size = 64  # Batch size from the training process
+# input_seq_length = 5  # Maximum length of the input sequence
+# h = 8  # Number of self-attention heads
+# d_k = 64  # Dimensionality of the linearly projected queries and keys
+# d_v = 64  # Dimensionality of the linearly projected values
+# d_model = 512  # Dimensionality of the model sub-layers' outputs
+# batch_size = 64  # Batch size from the training process
  
-queries = random.random((batch_size, input_seq_length, d_k))
-keys = random.random((batch_size, input_seq_length, d_k))
-values = random.random((batch_size, input_seq_length, d_v))
+# queries = random.random((batch_size, input_seq_length, d_k))
+# keys = random.random((batch_size, input_seq_length, d_k))
+# values = random.random((batch_size, input_seq_length, d_v))
  
-multihead_attention = MultiHeadAttention(h, d_k, d_v, d_model)
-print(multihead_attention(queries, keys, values))
+# multihead_attention = MultiHeadAttention(h, d_k, d_v, d_model)
+# print(multihead_attention(queries, keys, values))
