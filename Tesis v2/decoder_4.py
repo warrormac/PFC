51c364562519cf59bacf09081dd881a3fb5023fd
Andre Alvarez <andre.alvarez@ucsp.edu.pe>
2023-07-10 00:41:58 -0500

    Update decoder_4.py

--- Tesis v2/decoder_4.py
+++ Tesis v2/decoder_4.py
@@ -73,22 +73,22 @@ class Decoder(Layer):
     
     
 ################################# TEST ##################################
-from numpy import random
+# from numpy import random
 
-dec_vocab_size = 20  # Vocabulary size for the decoder
-input_seq_length = 5  # Maximum length of the input sequence
-h = 8  # Number of self-attention heads
-d_k = 64  # Dimensionality of the linearly projected queries and keys
-d_v = 64  # Dimensionality of the linearly projected values
-d_ff = 2048  # Dimensionality of the inner fully connected layer
-d_model = 512  # Dimensionality of the model sub-layers' outputs
-n = 6  # Number of layers in the decoder stack
+# dec_vocab_size = 20  # Vocabulary size for the decoder
+# input_seq_length = 5  # Maximum length of the input sequence
+# h = 8  # Number of self-attention heads
+# d_k = 64  # Dimensionality of the linearly projected queries and keys
+# d_v = 64  # Dimensionality of the linearly projected values
+# d_ff = 2048  # Dimensionality of the inner fully connected layer
+# d_model = 512  # Dimensionality of the model sub-layers' outputs
+# n = 6  # Number of layers in the decoder stack
 
-batch_size = 64  # Batch size from the training process
-dropout_rate = 0.1  # Frequency of dropping the input units in the dropout layers
+# batch_size = 64  # Batch size from the training process
+# dropout_rate = 0.1  # Frequency of dropping the input units in the dropout layers
 
-input_seq = random.random((batch_size, input_seq_length))
-enc_output = random.random((batch_size, input_seq_length, d_model))
+# input_seq = random.random((batch_size, input_seq_length))
+# enc_output = random.random((batch_size, input_seq_length, d_model))
 
-decoder = Decoder(dec_vocab_size, input_seq_length, h, d_k, d_v, d_model, d_ff, n, dropout_rate)
-print(decoder(input_seq, enc_output, None, True))
+# decoder = Decoder(dec_vocab_size, input_seq_length, h, d_k, d_v, d_model, d_ff, n, dropout_rate)
+# print(decoder(input_seq, enc_output, None, True))
