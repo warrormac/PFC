e42135957e71e670fe7710f0037244ab89f771be
Andre Alvarez <andre.alvarez@ucsp.edu.pe>
2023-07-10 00:41:37 -0500

    Update encoder_3.py

--- Tesis v2/encoder_3.py
+++ Tesis v2/encoder_3.py
@@ -93,19 +93,19 @@ class Encoder(Layer):
 ################################# TEST ##################################
 from numpy import random
 
-enc_vocab_size = 20 # Vocabulary size for the encoder
-input_seq_length = 5  # Maximum length of the input sequence
-h = 8  # Number of self-attention heads
-d_k = 64  # Dimensionality of the linearly projected queries and keys
-d_v = 64  # Dimensionality of the linearly projected values
-d_ff = 2048  # Dimensionality of the inner fully connected layer
-d_model = 512  # Dimensionality of the model sub-layers' outputs
-n = 6  # Number of layers in the encoder stack
+# enc_vocab_size = 20 # Vocabulary size for the encoder
+# input_seq_length = 5  # Maximum length of the input sequence
+# h = 8  # Number of self-attention heads
+# d_k = 64  # Dimensionality of the linearly projected queries and keys
+# d_v = 64  # Dimensionality of the linearly projected values
+# d_ff = 2048  # Dimensionality of the inner fully connected layer
+# d_model = 512  # Dimensionality of the model sub-layers' outputs
+# n = 6  # Number of layers in the encoder stack
 
-batch_size = 64  # Batch size from the training process
-dropout_rate = 0.1  # Frequency of dropping the input units in the dropout layers
+# batch_size = 64  # Batch size from the training process
+# dropout_rate = 0.1  # Frequency of dropping the input units in the dropout layers
 
-input_seq = random.random((batch_size, input_seq_length))
+# input_seq = random.random((batch_size, input_seq_length))
 
-encoder = Encoder(enc_vocab_size, input_seq_length, h, d_k, d_v, d_model, d_ff, n, dropout_rate)
-print(encoder(input_seq, None, True))
+# encoder = Encoder(enc_vocab_size, input_seq_length, h, d_k, d_v, d_model, d_ff, n, dropout_rate)
+# print(encoder(input_seq, None, True))
